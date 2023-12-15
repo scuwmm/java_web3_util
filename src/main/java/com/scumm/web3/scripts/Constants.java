078fc4fc36434ab9b31dc6fb0989db00bca22065
aaron <aaron@youxiang.io>
2023-12-15 14:08:15 +0800

    XX

---
+++ src/main/java/com/scumm/web3/scripts/Constants.java
@@ -0,0 +1,11 @@
+package com.scumm.web3.scripts;
+
+import java.math.BigInteger;
+
+public class Constants {
+
+    public static final int ONCE = 500;
+
+    public static final BigInteger GAS_LIMIT = BigInteger.valueOf(200000);
+
+}
