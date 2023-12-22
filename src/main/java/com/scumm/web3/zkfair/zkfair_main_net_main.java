fd7a66766e60660d8cb64572613b6c098819f843
aaron <aaron@youxiang.io>
2023-12-22 17:02:14 +0800

    zkfair

--- src/main/java/com/scumm/web3/zkfair/zkfair_main_net_main.java
+++ src/main/java/com/scumm/web3/zkfair/zkfair_main_net_main.java
@@ -1,5 +1,6 @@
 package com.scumm.web3.zkfair;
 
+import com.scumm.web3.scripts.Constants;
 import okhttp3.OkHttpClient;
 import org.apache.commons.collections4.properties.PropertiesFactory;
 import org.web3j.crypto.Credentials;
@@ -33,14 +34,22 @@ public class zkfair_main_net_main {
 
     static {
         try {
-            Properties props = PropertiesFactory.INSTANCE.load("src/main/resources/zkfair.properties");
+            Properties props = PropertiesFactory.INSTANCE.load("src/main/resources/zkfair_test.properties");
             NET_WORK = props.getProperty("NET_WORK");
             CHAIN_ID = Long.parseLong(props.getProperty("CHAIN_ID"));
 //            String PRIVATE_KEYS =props.getProperty("PRIVATE_KEYS");
-            pks = Arrays.asList(props.getProperty("PRIVATE_KEYS").split(","))
-                    .stream().map(pk -> Credentials.create(pk))
-                    .collect(Collectors.toMap(pk -> pk.getAddress(), pk -> pk));
             web3j = Web3j.build(new HttpService(NET_WORK, new OkHttpClient.Builder().build(), false));
+
+            pks = Arrays.asList(props.getProperty("PRIVATE_KEYS").split(","))
+                    .stream().map(pk -> {
+                        Credentials c = Credentials.create(pk);
+                        BigDecimal balance = Convert.fromWei(String.valueOf(getBalance(c.getAddress())), Convert.Unit.ETHER);
+                        System.out.println("address= " + c.getAddress() + " pk=" + pk + " balance=" + balance);
+                        return c;
+                    })
+                    .collect(Collectors.toMap(pk -> pk.getAddress(), pk -> pk));
+            System.out.println("Init end.");
+
         } catch (Exception e) {
             System.out.printf("error");
         }
@@ -52,7 +61,7 @@ public class zkfair_main_net_main {
         pks.forEach((address, credentials) -> {
             executor.submit(() -> {
 
-                int i = 2;
+                int i = 5;
                 while (true && i-- >= 0) {
                     try {
 //                        System.out.println("当前地址：" + address);
@@ -61,12 +70,15 @@ public class zkfair_main_net_main {
 //                        BigInteger nonce = getNonce(address);
 //                        System.out.println("当前nonce：" + nonce);
 
-                        BigInteger gasPrice = web3j.ethGasPrice().send().getGasPrice().multiply(BigInteger.valueOf(1L));
-                        System.out.println("gasPrice:" + gasPrice);
+                        //100000 , 10000
+//                        BigInteger gasPrice = web3j.ethGasPrice().send().getGasPrice().multiply(BigInteger.valueOf(1L));
+                        BigInteger gasPrice = BigInteger.valueOf(100000).multiply(Constants.GWEI);
+                        //5000000000
                         BigInteger gasLimit = BigInteger.valueOf(50000L);
+                        System.out.println("gasPrice:" + Convert.fromWei(gasPrice.toString(), Convert.Unit.ETHER) + ", max gas:" + Convert.fromWei(gasPrice.multiply(gasLimit).toString(), Convert.Unit.ETHER));
 
                         // 设置交易的金额
-                        BigDecimal amount = BigDecimal.ZERO; // Convert.toWei(new BigDecimal("0.1"), Convert.Unit.ETHER);
+                        BigDecimal amount = Convert.toWei(BigDecimal.valueOf(0.0001), Convert.Unit.ETHER); // Convert.toWei(new BigDecimal("0.1"), Convert.Unit.ETHER);
 
                         // 创建 RawTransactionManager
                         RawTransactionManager rawTransactionManager = new RawTransactionManager(web3j, credentials, CHAIN_ID, 60, 3000);
@@ -76,7 +88,7 @@ public class zkfair_main_net_main {
                         TransactionReceipt tx = transfer.sendFunds(address, amount, Convert.Unit.WEI, gasPrice, gasLimit).send();
                         System.out.println("tx:" + tx.getTransactionHash());
                     } catch (Exception e) {
-                        System.out.println("init address:" + address + " error:" + e.getMessage());
+                        System.out.println("address:" + address + " send tx error:" + e.getMessage());
                         try {
                             Thread.sleep(1000L);
                         } catch (Throwable err) {
@@ -102,9 +114,15 @@ public class zkfair_main_net_main {
         }
     }
 
-    private static BigInteger getBalance(String address) throws IOException {
-        BigInteger bn = web3j.ethBlockNumber().send().getBlockNumber();
-        return web3j.ethGetBalance(address, DefaultBlockParameter.valueOf(bn)).send().getBalance();
+    private static BigInteger getBalance(String address) {
+        try {
+            BigInteger bn = web3j.ethBlockNumber().send().getBlockNumber();
+            return web3j.ethGetBalance(address, DefaultBlockParameter.valueOf(bn)).send().getBalance();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
